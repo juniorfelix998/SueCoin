f0bb3a72d06b379a1006e3d32834aba27b08e01f
juniorfelix998 <juniorfelix998@gmail.com>
2021-05-07 15:38:32 +0300

    Added validation method for the blockchain

--- blockchain.py
+++ blockchain.py
@@ -91,4 +91,14 @@ def get_chain():
     return jsonify(response), 200
 
 
+@app.route('/is_valid', methods=['GET'])
+def is_valid_chain():
+    is_valid = blockchain.is_chain_valid(blockchain.chain)
+    if is_valid:
+        response = {'message': 'The Block Chain is Valid'}
+    else:
+        response = {'message': 'The Block Chain is NOT VALID'}
+    return jsonify(response), 200
+
+
 app.run(host='0.0.0.0', port=5000)
